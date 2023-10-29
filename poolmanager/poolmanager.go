5d088ef3347aaeb27276ceb47ec1682c9f55dab3
davaymne <davaymne.misc@gmail.com>
2023-10-29 01:33:10 +0100

    Create poolmanager.go

---
+++ poolmanager/poolmanager.go
@@ -0,0 +1,22 @@
+package poolmanager
+
+import (
+	"net/rpc"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
+type PoolManager struct {
+	client         *rpc.Client
+}
+
+func NewPoolManager(poolManager string) (*PoolManager, error) {
+	client, err := rpc.DialHTTP("tcp", poolManager)
+	if err != nil {
+		log.Error("Connection error: ", err)
+	}
+	log.Info("PoolManager initialized successfully", "PoolManager", poolManager)
+	return &PoolManager{
+		client:         client,
+	}, nil
+}
